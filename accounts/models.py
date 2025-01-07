26f4a4bfacf88814e60c827d1a99211ae217e08e
Gyl Monteiro <gyldeveloper@gmail.com>
2025-01-07 08:53:47 -0300

    ajusted label phone_number to blank and null equal True

--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     registration = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
     
-
 
     def __str__(self):
         return self.username
