82f228895d7d0db273406e155fe350946bae44b7
Gyl Monteiro <gyldeveloper@gmail.com>
2025-01-07 10:58:02 -0300

    started views to register the time work

--- attendances/views.py
+++ attendances/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def confirmation(request):
+    if request.method == "POST":
+        register = request.POST.get("register")
+        password = request.POST.get('password')
+        
+        return render(request, "confirmation.html")
+
+    return render(request, "confirmation.html")
