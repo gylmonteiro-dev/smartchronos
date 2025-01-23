67ee79f0fdac630959a0d425ff5f0f494e406abe
Gyl Monteiro <gyldeveloper@gmail.com>
2025-01-22 23:14:35 -0300

    feat: add point validation and record deletion functionalities
    - Implemented point validation functionality to mark work point records as valid.
    - Added functionality to delete work point records.
    - Ensured proper redirection and parameter handling for related views.

--- point_control/urls.py
+++ point_control/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('employees/', views.employee_list, name='employee-list'),
-    path('employees/<int:pk>/', views.list_time_records, name='list-time-records')
+    path('employees/<int:pk>/', views.list_time_records, name='list-time-records'),
+    path('employees/<int:pk>/validation/', views.record_point_validation, name='validation'),
+    path('employees/<int:pk>/delete/', views.record_point_delete, name='delete-point')
 ]
--- point_control/views.py
+++ point_control/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from accounts.models import CustomUser
 from attendances.models import WorkPointRecord
+from django.urls import reverse
 # Create your views here.
 
 
@@ -9,6 +10,22 @@ def employee_list(request):
     return render(request, 'employee_list.html', {'list_employees': list_employees})
 
 def list_time_records(request, pk):
+    user = CustomUser.objects.get(pk=pk)
     last_records = WorkPointRecord.objects.filter(user__pk=pk).order_by('-created_at')[:5]
     
-    return render(request, 'time_records_list.html', {'last_records': last_records})
+    return render(request, 'time_records_list.html', {'last_records': last_records, 'user': user})
+
+def record_point_validation(request, pk):
+    point_record = get_object_or_404(WorkPointRecord, pk=pk)
+    point_record.valid = True
+    point_record.save()
+
+    user_pk = point_record.user.id
+    print(user_pk)
+    return redirect(reverse("list-time-records", args=[user_pk]))
+
+def record_point_delete(request ,pk):
+    point_record = get_object_or_404(WorkPointRecord, pk=pk)
+    user_pk = point_record.user.id
+    point_record.delete()
+    return redirect(reverse("list-time-records", args=[user_pk]))
